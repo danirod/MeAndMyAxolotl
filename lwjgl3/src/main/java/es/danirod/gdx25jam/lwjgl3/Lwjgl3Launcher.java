8d31d04a20e95a905333e8eb59996f86a8e0504a
Dani Rodríguez <dani@danirod.es>
2023-06-25 04:25:01 +0200

    Release

--- lwjgl3/src/main/java/es/danirod/gdx25jam/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/es/danirod/gdx25jam/lwjgl3/Lwjgl3Launcher.java
@@ -17,7 +17,7 @@ public class Lwjgl3Launcher {
 
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
         Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
-        configuration.setTitle("libGDX-Jam-25");
+        configuration.setTitle("ME AND MY AXOLOTL");
         configuration.useVsync(true);
         //// Limits FPS to the refresh rate of the currently active monitor.
         configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate);
